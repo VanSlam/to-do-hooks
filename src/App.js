cfab9f1f96fdbec61d6fadb5d99d955c9027e386
Zachary Van Slambrook <zvanslambrook@yahoo.com>
2019-07-16 11:50:47 -0400

    Add complete function that strikes thru text.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { useState } from 'react'
 import './App.css'
 
-function Todo({ todo, index }) {
-  return <div className="todo">{todo.text}</div>
+function Todo({ todo, index, completeTodo }) {
+  //ternary, strikes through text when completed. if complete line through, else do nothing
+  return (
+    <div
+      style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}
+      className="todo"
+    >
+      {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Completed</button>
+      </div>
+    </div>
+  )
 }
 
 function TodoForm({ addTodo }) {
@@ -20,6 +31,7 @@ function TodoForm({ addTodo }) {
         type="text"
         className="intput"
         value={value}
+        placeholder="Add task..."
         onChange={e => setValue(e.target.value)}
       />
     </form>
@@ -42,11 +54,23 @@ function App() {
     const newTodos = [...todos, { text }]
     setTodos(newTodos)
   }
+
+  const completeTodo = index => {
+    const newTodos = [...todos]
+    newTodos[index].isCompleted = true
+    setTodos(newTodos)
+  }
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+          />
         ))}
         <TodoForm addTodo={addTodo} />
       </div>
